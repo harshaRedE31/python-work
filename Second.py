de840a510f224f1a30b354ce5d6f886e7f0dba14
harshaRedE31 <harshaoffl31@gmail.com>
2026-01-30 12:53:53 +0530

    sec

--- Second.py
+++ Second.py
@@ -1,5 +1,27 @@
 name="harsha"
 age=22
 height=6.1
+single=True
 
 print(f"I am {name}, my age is {age} and, my height is {height}")
+
+print(type(name))
+print(type(height))
+print(type(single))
+
+a=9
+b=9
+sum=a+b
+print(sum)
+
+"""
+this is comment
+and it is multilinecomment
+"""
+
+#this single line comment
+
+u=int("2")
+o=8
+print(type(u))
+print(u+o)
