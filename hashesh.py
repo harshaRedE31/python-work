0e69344b7a8332be2ba52c2cb879121a0b32e956
harshaRedE31 <harshaoffl31@gmail.com>
2026-02-06 14:23:54 +0530

    .

---
+++ hashesh.py
@@ -0,0 +1,19 @@
+#store frequenc 
+#nums = [5,6,7,7,1,9,111,1,1,5,1,1]
+# freq_map = {}
+# for i in range(0, len(nums)):
+#   if nums[i] in freq_map:
+#     freq_map[nums[i]] +=1
+#   else:
+#     freq_map[nums[i]]=1
+# print(freq_map)
+
+# hashmap = {}
+# n = len(nums)
+# for i in range(0, n):
+#   hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
+# print(hashmap)      
+
+# n=[5,3,2,2,1,5,5,7,5,10]
+# m=[10,11,111,1,9,5,8,59,2] 
+               
